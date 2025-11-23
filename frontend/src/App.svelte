7cd9f206131e766f3d02da570aa5255d57b35081
Pluvic <victorpluvinage25@gmail.com>
2025-11-23 15:15:23 +0100

    Update Result

--- frontend/src/App.svelte
+++ frontend/src/App.svelte
@@ -17,8 +17,10 @@
     <DragDrop onAnalyze={handleUpload} />
 
 	{#if result}
+    <div class = "PopUpResult">
 		<h2>Result :</h2>
 		<pre>{JSON.stringify(result, null, 2)}</pre>
+    </div>
 	{/if}
 </main>
 
@@ -39,5 +41,20 @@
     h1 {
         color:white;
         padding: 0.5em;
+    }
+    
+    .PopUpResult {
+        position: absolute;
+        top: 10em;
+        left: 50%;
+        transform: translateX(-50%);
+        background-color: rgb(255, 255, 255);
+        border: 2px solid rgb(0, 0, 0);
+        border-radius: 8px;
+        padding: 1em;
+        max-width: 80%;
+        max-height: 60vh;
+        overflow-y: auto;
+        box-shadow: 0 4px 12px rgba(0, 0, 0, 0.1);
     }
 </style>
